b40df9757f32229accdf1a899a1e27d737b23f2d
liubailin2017 <liubailinprivate@163.com>
2018-04-30 19:00:16 +0800

    更新MyPath

--- app/src/main/java/com/sto/asportclient/data/util/MyPath.java
+++ app/src/main/java/com/sto/asportclient/data/util/MyPath.java
@@ -1,64 +1,95 @@
 package com.sto.asportclient.data.util;
+import android.support.annotation.NonNull;
+
+import java.util.Date;
 import java.util.List;
 /**
  * 路径定义
  * @author liubailin
  */
 public class MyPath {
-	
-	public static class PositionPoint { 
-		private double x;
-		private double y;
-		private float t;
-		
-		public PositionPoint() {
-			super();
-		}
-		public PositionPoint(double x, double y, float t) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.t = t;
-		}
+    /**
+     * 单个点坐标
+     */
+    public static class PositionPoint {
+        private double longitude;   //经度
+        private double latitude;    //纬度
+        private float t; //与上一点相隔时间 单位： s 如果第一个点，t = 0;
 
-		public double getX() {
-			return x;
-		}
-		public void setX(double x) {
-			this.x = x;
-		}
-		public double getY() {
-			return y;
-		}
-		public void setY(double y) {
-			this.y = y;
-		}
-		public float getT() {
-			return t;
-		}
-		public void setT(float t) {
-			this.t = t;
-		}
-		@Override
-		public String toString() {
-			return "PositionPoint [x=" + x + ", y=" + y + ", t=" + t + "]";
-		} 
-		
-	}
-	
+        public PositionPoint(double longitude, double latitude) {
+            this(longitude,latitude,0);
+        }
+
+        public PositionPoint(double longitude, double latitude, float t) {
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.t = t;
+        }
+
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(double longitude) {
+            this.longitude = longitude;
+        }
+
+        public double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(double latitude) {
+            this.latitude = latitude;
+        }
+
+        public float getT() {
+            return t;
+        }
+
+        public void setT(float t) {
+            this.t = t;
+        }
+
+        @Override
+        public String toString() {
+            return "PositionPoint{" +
+                    "longitude=" + longitude +
+                    ", latitude=" + latitude +
+                    ", t=" + t +
+                    '}';
+        }
+    }
+
+    private Date date; //开始时间
 	private List<PositionPoint> path;
-	
-	public List<PositionPoint> getPath() {
+
+
+    public MyPath(Date date, List<PositionPoint> path) {
+        this.date = date;
+        this.path = path;
+    }
+
+    public List<PositionPoint> getPath() {
 		return path;
 	}
 
-	public void setPath(List<PositionPoint> path) {
-		this.path = path;
-	}
+    public void setPath(@NonNull  List<PositionPoint> path) {
+        this.path = path;
+    }
 
-	@Override
-	public String toString() {
-		return "MyPath [path=" + path + "]";
-	}
-	
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(@NonNull Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "MyPath{" +
+                "date=" + date +
+                ", path=" + path +
+                '}';
+    }
 }
